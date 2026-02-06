afaccdd3561988192be537b3a3e01e4b9261b6a5
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-02-06 16:04:06 -0300

    feat(navigation): add navbar component with responsive layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { LancamentosPesquisa } from './lancamentos-pesquisa/lancamentos-pesquisa';
 import { TableModule } from 'primeng/table';
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
   imports: [
     LancamentosPesquisa,
-    TableModule
+    TableModule,
+    Navbar
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
