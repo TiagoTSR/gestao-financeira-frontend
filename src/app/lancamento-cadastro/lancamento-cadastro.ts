e287f5bf56b69e7949aaf613f0989d37e2e48a86
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-02-07 16:06:02 -0300

    feat(lancamento): add Angular Material Datepicker to cadastro form

--- src/app/lancamento-cadastro/lancamento-cadastro.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.ts
@@ -4,6 +4,11 @@ import { ButtonModule } from 'primeng/button';
 import { FluidModule } from 'primeng/fluid';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-lancamento-cadastro',
@@ -12,11 +17,18 @@ import { InputTextModule } from 'primeng/inputtext';
     ButtonModule,
     FluidModule,
     InputNumberModule,
-    InputTextModule
+    InputTextModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule
   ],
   templateUrl: './lancamento-cadastro.html',
   styleUrl: './lancamento-cadastro.scss',
 })
 export class LancamentoCadastro {
 
+  dataVencimento: Date | undefined;
+  dataRecebimento: Date | undefined;
+  
 }
