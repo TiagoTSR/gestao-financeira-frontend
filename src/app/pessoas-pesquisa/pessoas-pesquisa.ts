efa3b80ed20ed3e379e572b216b66f20cf264d05
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-02-07 15:00:52 -0300

    feat(people): add people data table with mock data

--- src/app/pessoas-pesquisa/pessoas-pesquisa.ts
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-pessoas-pesquisa',
   imports: [
     ButtonModule,
-    InputTextModule
+    InputTextModule,
+    TableModule,
+    CommonModule
   ],
   templateUrl: './pessoas-pesquisa.html',
   styleUrl: './pessoas-pesquisa.scss',
 })
 export class PessoasPesquisa {
+
+  pessoas = [
+    { nome: 'Manoel Pinheiro', cidade: 'Uberlândia', estado: 'MG', ativo: true },
+    { nome: 'Sebastião da Silva', cidade: 'São Paulo', estado: 'SP', ativo: false },
+    { nome: 'Carla Souza', cidade: 'Florianópolis', estado: 'SC', ativo: true },
+    { nome: 'Luís Pereira', cidade: 'Curitiba', estado: 'PR', ativo: true },
+    { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro', estado: 'RJ', ativo: false },
+    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', ativo: true }
+  ];
 
 }
