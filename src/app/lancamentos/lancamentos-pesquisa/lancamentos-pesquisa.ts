29e75a6e4b3f05482018fae27b7557255069ccf7
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-02-16 08:58:56 -0300

    feat(lancamento): replace mock data with API call in search component

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { Lancamento } from '../../models/lancamento.model';
+import { LancamentoService } from '../lancamento';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
@@ -17,21 +19,38 @@ import { ButtonModule } from 'primeng/button';
 })
 export class LancamentosPesquisa {
 
-  lancamentos = [
-      { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-        dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
-      { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/06/2017',
-        dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil' },
-      { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '20/07/2017',
-        dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda' },
-      { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: '05/06/2017',
-        dataPagamento: '30/05/2017', valor: 800, pessoa: 'Escola Abelha Rainha' },
-      { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: '18/08/2017',
-        dataPagamento: null, valor: 55000, pessoa: 'Sebastião Souza' },
-      { tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '10/07/2017',
-        dataPagamento: '09/07/2017', valor: 1750, pessoa: 'Casa Nova Imóveis' },
-      { tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
-        dataPagamento: null, valor: 180, pessoa: 'Academia Top' }
-    ];
+  lancamentos: Lancamento[] = [];
+  totalRegistros = 0;
+  loading = false;
 
+  constructor(private lancamentoService: LancamentoService) {}
+
+  ngOnInit(): void {
+    this.pesquisar();
+  }
+
+  pesquisar(page = 0, size = 0): void {
+    this.loading = true;
+
+    this.lancamentoService.findAll({
+      page,
+      size
+    }).subscribe({
+      next: response => {
+        this.lancamentos = response.content;
+        this.totalRegistros = response.totalElements;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+
+  aoMudarPagina(event: any): void {
+    const page = event.first / event.rows;
+    const size = event.rows;
+
+    this.pesquisar(page, size);
+  }
 }
