62897d3e989d0e1d74d252fcc9d964cdf6a5e306
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-02-17 15:51:30 -0300

    feat(navigation): link menu items to lancamentos and pessoas routes

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
@@ -13,4 +14,16 @@ import { CommonModule } from '@angular/common';
 export class Navbar {
   
   exibindoMenu: boolean = false;
+
+  constructor(
+    private router: Router
+  ) {}
+
+  Lancamentos(): void {
+  this.router.navigate(['/admin/lancamentos']);
+  }
+  
+  Pessoas(): void {
+  this.router.navigate(['/admin/pessoas']);
+  }
 }
