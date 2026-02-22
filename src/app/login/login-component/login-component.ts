3eed2e92c18965832a03d95e75b3e772cdf2957e
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-02-22 15:20:18 -0300

    feat(auth): connect login component to service and handle navigation

--- src/app/login/login-component/login-component.ts
+++ src/app/login/login-component/login-component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../AuthService';
+import { Login } from '../../models/login.model';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login-component',
@@ -10,18 +13,72 @@ import { FormsModule } from '@angular/forms';
 })
 export class loginComponent {
 
-  usuario!: string;
-  senha!: string;
-
   router = inject(Router);
+  authService = inject(AuthService);
 
-  Login() {
+  loading = false;
 
-    if (this.usuario === 'admin@ll' && this.senha === 'admin') {
+  login: Login = {
+    username: '',
+    password: ''
+  };
+
+  constructor(){
+    this.authService.removerToken();
+  }
+
+  logar() {
+  if (!this.validarFormulario()) {
+    return;
+  }
+
+  this.loading = true;
+
+  this.authService.logar(this.login).subscribe({
+    next: (res) => { 
+      this.loading = false;
+
+      if (res && res.token) {
+        this.authService.addToken(res.token); 
+      }
+
+      this.gerarToast().fire({ icon: "success", title: "Seja bem-vindo!" });
       this.router.navigate(['/admin/lancamentos']);
-    } else {
-      alert('Usuário ou senha inválidos!');
+    },
+    error: (erro) => {
+      this.loading = false;
+    
+      Swal.fire('Usuário ou senha incorretos!', '', 'error');
     }
+  });
 }
+
+  validarFormulario(): boolean {
+    if (!this.login.username || !this.login.username.trim()) {
+      Swal.fire('Campo obrigatório', 'Por favor, informe seu usuário', 'warning');
+      return false;
+    }
+
+    if (!this.login.password || !this.login.password.trim()) {
+      Swal.fire('Campo obrigatório', 'Por favor, informe sua senha', 'warning');
+      return false;
+    }
+
+    return true;
+  }
+
+  gerarToast() {
+    return Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+  }
 
 }
